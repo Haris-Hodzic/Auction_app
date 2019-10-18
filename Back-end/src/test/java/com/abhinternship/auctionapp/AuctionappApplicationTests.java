0c9f8893f92e79e22715ca2ec19f6e397b3d2d9e
Haris Hodzic <haris.cyber03@gmail.com>
2019-10-18 12:32:53 +0200

    Back-end app created

---
+++ Back-end/src/test/java/com/abhinternship/auctionapp/AuctionappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinternship.auctionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
