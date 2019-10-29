555693444cb4c8608045b6108d6d49dfac061c06
Haris Hodzic <haris.hodzic.03@gmail.com>
2019-10-29 10:01:21 +0100

    Validation

--- Front-end/Auction_App_Front/app/routes/login.js
+++ Front-end/Auction_App_Front/app/routes/login.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import User from '../models/user';
 
 export default Route.extend({
+<<<<<<< HEAD
   model() {
     return this.store.createRecord('luser');
   }
