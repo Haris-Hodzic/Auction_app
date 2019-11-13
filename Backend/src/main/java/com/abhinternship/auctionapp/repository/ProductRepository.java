14820f637be92e83ff1bf7831af0f46c1c10aca4
Haris Hodzic <haris.hodzic.03@gmail.com>
2019-11-13 16:34:04 +0100

    Bidding process

--- Backend/src/main/java/com/abhinternship/auctionapp/repository/ProductRepository.java
+++ Backend/src/main/java/com/abhinternship/auctionapp/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package com.abhinternship.auctionapp.repository;
 import com.abhinternship.auctionapp.model.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
